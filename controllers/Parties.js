76bc3240b5c1a4cfce7f60bde0fb35e8125d6128
GedeonV <zinni.arthur@gmail.com>
2020-08-25 11:27:43 +0200

    Ajout update state

--- controllers/Parties.js
+++ controllers/Parties.js
@@ -134,8 +134,8 @@ exports.parties_status = (req,res) => {
 	Party.findOneAndUpdate({_id : req.params._id},partyStatus)
 	.then(party => {
 		if(party){
-			console.log(party)
-			/*if(party.state == 1){
+			/*console.log(party)
+			if(party.state == 1){
 				res.json({'notification': 'Evènement démarré'})
 			} else if (party.state == 2){
 				res.json({'notification': 'Evènement terminé'})
