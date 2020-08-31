805072e2dfffb70daddd62a6c8c28a9210556730
GedeonV <zinni.arthur@gmail.com>
2020-08-31 12:11:42 +0200

    Changement route delete -> post

--- controllers/Parties.js
+++ controllers/Parties.js
@@ -1,188 +1,199 @@
-const cors = require("cors")
+const cors = require("cors");
 const Party = require("../models/Party");
 const User = require("../models/User");
 
-exports.parties_create = (req,res) => {
-	const today = new Date()
-	const partyData = {
-		event_name: req.body.event_name,
-		date: req.body.date,
-		location: req.body.location,
-		description: req.body.description,
-		state: req.body.state,
-		event_code: req.body.event_code,
-		theme: req.body.theme,
-		current_song: req.body.current_song,
-		current_user: req.body.current_user,
-		created: today
-	}
-	Party.findOne({
-		event_name: req.body.event_name
-	})
-	.then(party => {
-		if(!party){
-			Party.create(partyData)
-				.then(party => {
-					res.json({status: party.event_name + '  enregistré'})
-				})
-				.catch(err => {
-					res.json({error: err})
-				})
-			}
-		else{
-			res.json({error: 'Un évènement porte déjà ce nom'})
-		}
-	})
-	.catch(err => {
-		res.json({'error': err})
-	})
-}
+exports.parties_create = (req, res) => {
+  const today = new Date();
+  const partyData = {
+    event_name: req.body.event_name,
+    date: req.body.date,
+    location: req.body.location,
+    description: req.body.description,
+    state: req.body.state,
+    event_code: req.body.event_code,
+    theme: req.body.theme,
+    current_song: req.body.current_song,
+    current_user: req.body.current_user,
+    created: today,
+  };
+  Party.findOne({
+    event_name: req.body.event_name,
+  })
+    .then((party) => {
+      if (!party) {
+        Party.create(partyData)
+          .then((party) => {
+            res.json({ status: party.event_name + "  enregistré" });
+          })
+          .catch((err) => {
+            res.json({ error: err });
+          });
+      } else {
+        res.json({ error: "Un évènement porte déjà ce nom" });
+      }
+    })
+    .catch((err) => {
+      res.json({ error: err });
+    });
+};
 
-exports.parties_get_all = (req,res) => {
-	Party.find({})
-	.then(party => {
-		if(party){
-			res.send(party)
-		}else{
-			res.json({error: 'Aucune donnée'})
-		}
-	})
-	.catch(err => {
-		res.json({'error': err})
-	})
-}
+exports.parties_get_all = (req, res) => {
+  Party.find({})
+    .then((party) => {
+      if (party) {
+        res.send(party);
+      } else {
+        res.json({ error: "Aucune donnée" });
+      }
+    })
+    .catch((err) => {
+      res.json({ error: err });
+    });
+};
 
-exports.parties_get_id = (req,res) => {
-	Party.findOne({
-		_id : req.params._id
-	})
-	.populate('users')
-	.then(party => {
-		if(party){
-				let data_json = 
-					{
-						"party_id": party._id,
-						"event_name" : party.event_name,
-						"description": party.description,
-						"location" : party.location,
-						"date": party.date,
-						"state": party.state,
-						"event_code": party.event_code,
-						"theme": party.theme,
-						"users": party.users,
-						"current_song" : party.current_song,
-						"current_user" : party.current_user,
-						"created_at" : party.created_at,
-					}
-				res.send(data_json)
-		}else{
-			res.json({'erreur': 'L\'événement n\'existe pas'})
-		}
-	})
-	.catch(err => {
-		res.json({'erreur': err})
-	})
-}
+exports.parties_get_id = (req, res) => {
+  Party.findOne({
+    _id: req.params._id,
+  })
+    .populate("users")
+    .then((party) => {
+      if (party) {
+        let data_json = {
+          party_id: party._id,
+          event_name: party.event_name,
+          description: party.description,
+          location: party.location,
+          date: party.date,
+          state: party.state,
+          event_code: party.event_code,
+          theme: party.theme,
+          users: party.users,
+          current_song: party.current_song,
+          current_user: party.current_user,
+          created_at: party.created_at,
+        };
+        res.send(data_json);
+      } else {
+        res.json({ erreur: "L'événement n'existe pas" });
+      }
+    })
+    .catch((err) => {
+      res.json({ erreur: err });
+    });
+};
 
-exports.parties_delete = (req,res) => {
-	Party.findOneAndRemove({
-		_id: req.params._id
-	})
-	.then(party => {
-		if(party){
-			res.send({'notification': 'Evénèment supprimé'})
-		}else{
-			res.json({'erreur': 'Impossible de supprimé'})
-		}
-	})
-	.catch(err => {
-		res.json({'erreur': err})
-	})
-}
+exports.parties_delete = (req, res) => {
+  Party.findOneAndRemove({
+    _id: req.params._id,
+  })
+    .then((party) => {
+      if (party) {
+        res.send({ notification: "Evénèment supprimé" });
+      } else {
+        res.json({ erreur: "Impossible de supprimé" });
+      }
+    })
+    .catch((err) => {
+      res.json({ erreur: err });
+    });
+};
 
-exports.parties_update = (req,res) => {
-	const today = new Date()
-	const partyUpdatedData = {
-		event_name: req.body.event_name,
-		date: req.body.date,
-		location: req.body.location,
-		description: req.body.description,
-		state: req.body.state,
-		event_code: req.body.event_code,
-		theme: req.body.theme,
-		updated_at: today,
-	}
-	Party.findOneAndUpdate({
-		_id : req.params._id
-	}, partyUpdatedData)
-	.then(party => {
-		if(party){
-			res.send({'notification': 'Evènement modifié'})
-		}else{
-			res.json({'erreur': 'Impossible de mettre à jour'})
-		}
-	})
-	.catch(err => {
-		res.json({'erreur': err})
-	})
-}
+exports.parties_update = (req, res) => {
+  const today = new Date();
+  const partyUpdatedData = {
+    event_name: req.body.event_name,
+    date: req.body.date,
+    location: req.body.location,
+    description: req.body.description,
+    state: req.body.state,
+    event_code: req.body.event_code,
+    theme: req.body.theme,
+    updated_at: today,
+  };
+  Party.findOneAndUpdate(
+    {
+      _id: req.params._id,
+    },
+    partyUpdatedData
+  )
+    .then((party) => {
+      if (party) {
+        res.send({ notification: "Evènement modifié" });
+      } else {
+        res.json({ erreur: "Impossible de mettre à jour" });
+      }
+    })
+    .catch((err) => {
+      res.json({ erreur: err });
+    });
+};
 
-exports.parties_status = (req,res) => {
-	Party.findOneAndUpdate({_id : req.params._id},{state: req.body.state})
-	.then(party => {
-		if(party){
-			if(req.body.state == 1){
-				res.json({'notification': 'Evènement démarré'})
-			} else if (req.body.state == 2){
-				res.json({'notification': 'Evènement terminé'})
-			} else if(req.body.state == 0){
-				res.json({'notification': 'Evènement non commencé'})
-			} else if(req.body.state > 2){
-				res.json({'notification': 'Etat inconnu'})
-			}
-		}else{
-			res.json({'erreur': 'Impossible de changer l\'état'})
-		}
-	})
-	.catch(err => {
-		res.json({'erreur': err})
-	})
-}
+exports.parties_status = (req, res) => {
+  Party.findOneAndUpdate({ _id: req.params._id }, { state: req.body.state })
+    .then((party) => {
+      if (party) {
+        if (req.body.state == 1) {
+          res.json({ notification: "Evènement démarré" });
+        } else if (req.body.state == 2) {
+          res.json({ notification: "Evènement terminé" });
+        } else if (req.body.state == 0) {
+          res.json({ notification: "Evènement non commencé" });
+        } else if (req.body.state > 2) {
+          res.json({ notification: "Etat inconnu" });
+        }
+      } else {
+        res.json({ erreur: "Impossible de changer l'état" });
+      }
+    })
+    .catch((err) => {
+      res.json({ erreur: err });
+    });
+};
 
-exports.parties_signup = (req,res) => {
-	Party.findOneAndUpdate({
-		_id : req.params._id
-	},{$addToSet: {users: req.body.userId}})
-	.then(party => {
-		User.findOneAndUpdate({ _id : req.body.userId},{$addToSet: {parties: req.params._id}})
-		.then(party => {
-			if(party){
-				res.json({'notification': 'Utilisateur inscrit'})
-			}else{
-				res.json({'erreur': 'Impossible d\'inscrire l\'utilisateur'})
-			}
-		})
-	})
-	.catch(err => {
-		res.json({'erreur': err})
-	})
-}
+exports.parties_signup = (req, res) => {
+  Party.findOneAndUpdate(
+    {
+      _id: req.params._id,
+    },
+    { $addToSet: { users: req.body.userId } }
+  )
+    .then((party) => {
+      User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $addToSet: { parties: req.params._id } }
+      ).then((party) => {
+        if (party) {
+          res.json({ notification: "Utilisateur inscrit" });
+        } else {
+          res.json({ erreur: "Impossible d'inscrire l'utilisateur" });
+        }
+      });
+    })
+    .catch((err) => {
+      res.json({ erreur: err });
+    });
+};
 
-exports.parties_user_remove = (req,res) => {
-	Party.findOneAndUpdate({
-		_id : req.params._id
-	}, {$pull: {users: req.body.userId}})
-	.then(party => {
-		User.findOneAndUpdate({ _id : req.body.userId},{$pull: {parties: req.params._id}})
-		.then(party => {
-			if(party){
-				res.send({'notification': 'Utilisateur enlevé'})
-			}else{
-				res.json({'erreur': 'Impossible d\'enlever l\'utilisateur'})
-			}
-		})
-	})
-	.catch(err => {
-		res.json({'erreur': err})
-	})
-}
+exports.parties_user_remove = (req, res) => {
+  Party.findOneAndUpdate(
+    {
+      _id: req.params._id,
+    },
+    { $pull: { users: req.body.userId } }
+  )
+    .then((party) => {
+      User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $pull: { parties: req.params._id } }
+      ).then((party) => {
+        if (party) {
+          res.send({ notification: "Utilisateur enlevé" });
+        } else {
+          res.json({ erreur: "Impossible d'enlever l'utilisateur" });
+        }
+      });
+    })
+    .catch((err) => {
+      res.json({ erreur: err });
+    });
+};
--- routes/Parties.js
+++ routes/Parties.js
@@ -1,31 +1,35 @@
-const express = require("express")
-const parties = express.Router()
-const cors = require("cors")
+const express = require("express");
+const parties = express.Router();
+const cors = require("cors");
 //const jwt = require("jsonwebtoken")
 //const bcrypt = require("bcrypt")
 
-const Party = require("../models/Party")
-parties.use(cors())
+const Party = require("../models/Party");
+parties.use(cors());
 
-process.env.SECRET_KEY = 'sfmprj88'
-const checkAuth = require('../middleware/check-auth')
+process.env.SECRET_KEY = "sfmprj88";
+const checkAuth = require("../middleware/check-auth");
 
-const PartiesController = require("../controllers/Parties")
+const PartiesController = require("../controllers/Parties");
 
-parties.post("/create", checkAuth,PartiesController.parties_create)
+parties.post("/create", checkAuth, PartiesController.parties_create);
 
-parties.get('/', checkAuth,PartiesController.parties_get_all)
+parties.get("/", checkAuth, PartiesController.parties_get_all);
 
-parties.get('/event/:_id', checkAuth,PartiesController.parties_get_id)
+parties.get("/event/:_id", checkAuth, PartiesController.parties_get_id);
 
-parties.delete('/:_id', checkAuth,PartiesController.parties_delete)
+parties.delete("/:_id", checkAuth, PartiesController.parties_delete);
 
-parties.put('/event/:_id', checkAuth,PartiesController.parties_update)
+parties.put("/event/:_id", checkAuth, PartiesController.parties_update);
 
-parties.put('/event/:_id/state', checkAuth,PartiesController.parties_status)
+parties.put("/event/:_id/state", checkAuth, PartiesController.parties_status);
 
-parties.post('/event/:_id/sign', checkAuth,PartiesController.parties_signup)
+parties.post("/event/:_id/sign", checkAuth, PartiesController.parties_signup);
 
-parties.delete('/event/:_id/remove', checkAuth,PartiesController.parties_user_remove)
+parties.post(
+  "/event/:_id/remove",
+  checkAuth,
+  PartiesController.parties_user_remove
+);
 
-module.exports = parties
+module.exports = parties;
