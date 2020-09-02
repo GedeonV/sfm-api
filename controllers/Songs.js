16e9b47f0b3a4ab6e5ef377b200730dbd21b9aab
GedeonV <zinni.arthur@gmail.com>
2020-09-02 10:44:17 +0200

    test cloudinary

--- controllers/Songs.js
+++ controllers/Songs.js
@@ -1,6 +1,5 @@
 const cors = require("cors");
 const Song = require("../models/Song");
-const songs = require("../routes/Songs");
 
 exports.songs_upload = (req, res) => {
   console.log(req.file);
@@ -15,6 +14,30 @@ exports.songs_upload = (req, res) => {
     path: req.file.path,
     created: today,
   };
+  const cloudinary = require("cloudinary").v2;
+
+  cloudinary.config({
+    cloud_name: "sfm88",
+    api_key: "928328299487922",
+    api_secret: "ml3tMM05ePIw8-z8zUYOhEw4nZs",
+  });
+
+  cloudinary.uploader.upload(
+    req.file.path,
+    { resource_type: "raw", public_id: `uploads/${req.file.originalname}` }, // directory and tags are optional
+    function (err, song) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("file uploaded to Cloudinary");
+        console.log(song);
+        // remove file from server
+        const fs = require("fs");
+        fs.unlinkSync(req.file.path);
+      }
+    }
+  );
+
   Song.findOne({
     title: req.body.title,
   })
--- server.js
+++ server.js
@@ -1,13 +1,15 @@
-var express = require("express")
-var cors = require("cors")
-var bodyParser = require("body-parser")
-var app = express()
-var mongoose = require("mongoose")
-var port = process.env.PORT || 5000
+var express = require("express");
+var cors = require("cors");
+var bodyParser = require("body-parser");
+var app = express();
+var mongoose = require("mongoose");
+var port = process.env.PORT || 5000;
+var cloudinary = require("cloudinary").v2;
 
-app.use(bodyParser.json())
-app.use(cors())
-app.use(bodyParser.urlencoded({ extended : false}))
+app.use("/uploads", express.static("uploads"));
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 /*app.use(function(req, res, next) {
   var allowedOrigins = ['https://prj-redsquare.herokuapp.com/','https://redsquare-prj.netlify.com','http://127.0.0.1:8080','http://localhost:8080'];
@@ -22,20 +24,28 @@ app.use(bodyParser.urlencoded({ extended : false}))
   return next();
 });*/
 
-const mongoURI = 'mongodb://arthur_88:prjsfm88@singforme-shard-00-00.ybbr4.mongodb.net:27017,singforme-shard-00-01.ybbr4.mongodb.net:27017,singforme-shard-00-02.ybbr4.mongodb.net:27017/project-sfm?ssl=true&replicaSet=atlas-mwe0ks-shard-0&authSource=admin&retryWrites=true&w=majority'
+const mongoURI =
+  "mongodb://arthur_88:prjsfm88@singforme-shard-00-00.ybbr4.mongodb.net:27017,singforme-shard-00-01.ybbr4.mongodb.net:27017,singforme-shard-00-02.ybbr4.mongodb.net:27017/project-sfm?ssl=true&replicaSet=atlas-mwe0ks-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-mongoose.connect(mongoURI, {useNewUrlParser: true})
-	.then(() => console.log("MongoDB Connected"))
-	.catch(err => console.log(err))
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
-var Users = require("./routes/Users")
-var Songs = require("./routes/Songs")
-var Parties = require ("./routes/Parties")
+var Users = require("./routes/Users");
+var Songs = require("./routes/Songs");
+var Parties = require("./routes/Parties");
 
-app.use("/users", Users)	
-app.use("/songs", Songs)
-app.use("/parties", Parties)
+app.use("/users", Users);
+app.use("/songs", Songs);
+app.use("/parties", Parties);
 
-const server = app.listen(port, function (){
-	console.log("Server is running on port: " + port)
-})
+const server = app.listen(port, function () {
+  console.log("Server is running on port: " + port);
+});
+
+cloudinary.config({
+  cloud_name: "sfm88",
+  api_key: "928328299487922",
+  api_secret: "ml3tMM05ePIw8-z8zUYOhEw4nZs",
+});
