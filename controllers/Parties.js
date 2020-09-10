818065719002d2d2bed531a962ef425fbb68163b
GedeonV <zinni.arthur@gmail.com>
2020-09-10 15:57:14 +0200

    changement model parties

--- controllers/Parties.js
+++ controllers/Parties.js
@@ -211,7 +211,7 @@ exports.parties_signup = (req, res) => {
             parties: req.params._id,
             songs: {
               _id: req.params._id,
-              $each: { song: req.body.songId },
+              song: req.body.songId,
             },
           },
         }
