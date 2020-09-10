e04c7c529c1c5eac3f1889531a84c16fff59b7d3
GedeonV <zinni.arthur@gmail.com>
2020-09-10 15:23:55 +0200

    changement model parties

--- models/User.js
+++ models/User.js
@@ -41,10 +41,12 @@ const UserSchema = new Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "parties", // Relation avec la table Parties
       },
-      song: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "songs", // Relation avec la table Parties
-      },
+      song: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "songs", // Relation avec la table Parties
+        },
+      ],
     },
   ],
 
